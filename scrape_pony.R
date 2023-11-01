f7dd3a1042a375afde3ce6430745ecf44870d057
einGlasRotwein <juliane.tkotz@gmail.com>
2023-11-01 13:06:11 +0100

    edit google sheet
    add function to add scraped pony to Google sheet

--- scrape_pony.R
+++ scrape_pony.R
@@ -4,6 +4,7 @@
 
 library(rvest)
 library(tidyverse)
+library(googlesheets4)
 
 source("pony_scrape_function.R")
 
@@ -25,4 +26,27 @@ logged_in <- ep_session %>% session_submit(login)
 
 ## SCRAPE ----------------------------------------------------------------------
 
-pony_scraper("4592324", logged_in)
+pony_scraper("3087926", logged_in) # erwachsen eigen
+pony_scraper("4592324", logged_in) # fohlen eigen
+
+pony_scraper("2566804", logged_in) # erwachsen fremd
+pony_scraper("4539300", logged_in) # fohlen fremd
+
+pony_scraper("3827343", logged_in) # erwachsen händler
+pony_scraper("4360096", logged_in) # fohlen händler
+
+pony_scraper("4405124", logged_in) # fohlen pv
+
+pony_scraper("2719360", logged_in) # erwachsen auktion
+
+pony_scraper("2732991", logged_in)
+
+## ADD TO SHEET ----------------------------------------------------------------
+
+sheet_url <- "sheet_url" # enter google sheet id
+
+to_add <- pony_scraper("4599145", logged_in)
+
+# Log in with google account (can be pre-authorized via googlesheets4) and add 
+# new pony to sheet
+update_ponies(to_add, sheet_url, "ponies")
