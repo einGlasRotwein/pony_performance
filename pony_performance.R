df7d158c5443545e75169ae084e27b3ea86fd062
einGlasRotwein <julivnagel@gmail.com>
2024-03-09 18:20:06 +0100

    make script more flexible
    - enter discipline (and filter for it)
    - automatically show relevant horses based on ingame year

--- pony_performance.R
+++ pony_performance.R
@@ -10,7 +10,7 @@ custom_theme <-
     legend.position = "top"
   )
 
-sheet_url <- "sheet_url" # enter sheet url 
+sheet_url <- "sheet_url" # enter sheet url
 
 # Log in with google account (can be pre-authorized via googlesheets4)
 data <- read_sheet(sheet_url)
@@ -25,19 +25,18 @@ data <-
 # Plot all horses (height/skill) and highlight a single horse to show where the 
 # horse stands relative to the other horses.
 
-highlight_horse <- "Added Lead"
-discipline <- sym("superhorse") # or "trail" or "reining"
+highlight_horse <- "Maw Blood"
+disc <- "superhorse" # or "trail" or "reining"
 
 data %>% 
   mutate(highlight = ifelse(name %in% highlight_horse, TRUE, FALSE)) %>% 
-  # Exclude some Reining stallions that distort the plot
-  filter(!name %in% c("Rocher", "Charmed", "Shalimar", "Social Network", "Joey")) %>% 
-  ggplot(aes(x = height, y = !!discipline, colour = highlight, alpha = highlight)) +
+  filter(discipline == disc | name == highlight_horse) %>% 
+  ggplot(aes(x = height, y = !!sym(disc), colour = highlight, alpha = highlight)) +
   # height limits for registering Pintaloosa Ponys
   geom_vline(xintercept = 100) +
   geom_vline(xintercept = 140) +
   # mean values for height + potential
-  geom_hline(aes(yintercept = mean(!!discipline, na.rm = TRUE)), colour = "grey") +
+  geom_hline(aes(yintercept = mean(!!sym(disc), na.rm = TRUE)), colour = "grey") +
   geom_vline(aes(xintercept = mean(height)), colour = "grey") +
   geom_point(size = 2) +
   geom_smooth(method = "lm", se = FALSE) +
@@ -55,16 +54,18 @@ data %>%
 # would result in a foal that fulfills the criteria for being registered as 
 # Pintaloosa Pony
 
-dam_shetland <- 26.28
+ingame_year <- 2061
+dam_shetland <- 27.35
 
 discipline <- sym("superhorse") # or "trail" or "reining"
 
 data %>% 
   filter(
     sex == "stallion", 
-    breeding_year <= 2054,
+    breeding_year <= (ingame_year - 3),
     ## COLOUR FILTERS - UNCOMMENT AS NEEDED ##
     # grepl("E", extension),
+    # grepl("aa|At", agouti),
     # flaxen > 0,
     # grepl("SW", splashed_white),
     # grepl("Mu", mushroom),
@@ -79,7 +80,7 @@ data %>%
     # )
   ) %>%
   # Exclude some Reining stallions that distort the plot
-  filter(!name %in% c("Rocher", "Charmed", "Shalimar", "Social Network", "Joey")) %>% 
+  filter(!name %in% c("Charmed", "Social Network", "Joey")) %>%
   mutate(
     registration = 
       ifelse(
@@ -106,13 +107,13 @@ data %>%
 # Filter ponies that are of the right age and height to go on shows with 
 # children. Show those with the highest potential first.
 
-ingame_year <- 2057
-discipline <- sym("reining") # or "trail" or "reining"
+ingame_year <- 2060
+discipline <- sym("superhorse") # or "trail" or "reining"
 
 # Ponies for young children
 data %>% 
   filter(
-    breeding_year <= (2057 - 3),
+    breeding_year <= (ingame_year - 3),
     height <= 110
   ) %>% 
   arrange(desc(!!discipline)) %>% 
@@ -123,7 +124,7 @@ data %>%
 # should be above that threshold, so we don't filter for that)
 data %>% 
   filter(
-    breeding_year <= (2057 - 3),
+    breeding_year <= (ingame_year - 3),
     height <= 130
   ) %>% 
   arrange(desc(!!discipline)) %>% 
